10c343f41350ff810fb25b3b7f3ec62b33fe282f
Khalil <Khalil@Khalils-Mac.local>
2018-11-03 00:28:48 +0100

    Unit tests for Merge Join Algorithm added

--- src/HamaniKhalilTP4/HamaniKhalilMergeJoinDuplicate.java
+++ src/HamaniKhalilTP4/HamaniKhalilMergeJoinDuplicate.java
@@ -2,11 +2,47 @@ package HamaniKhalilTP4;
 
 import static HamaniKhalilTP4.SystemConfiguration.BUFFER_SIZE;
 
+import static HamaniKhalilTP4.SystemErrors.MESSAGE_ARRAY_EXCEEDED_BUFFER_SIZE;
+
+import java.io.File;
 import java.util.Arrays;
+
+import HamaniKhalilTP4.FileManager;
 
 public class HamaniKhalilMergeJoinDuplicate {
 	
-	public static char [] mergeJoinDuplicate(char [] r, char [] s) {
+	public static final void joinWithMergeJoin(String rFilename, String sFilename, String rsFilename) {
+		int	sReadLevel	= 0;
+		int	rReadLevel	= 0;
+		
+		char []	r	= FileManager.fileToArray(rFilename, rReadLevel);
+		char []	s	= FileManager.fileToArray(sFilename, sReadLevel);
+		
+		try {			
+			while(s != null) {
+				while(r != null) {
+					FileManager.arrayToFile(mergeJoinDuplicate(s, r), rsFilename);					
+					r	= FileManager.fileToArray(rFilename, ++ rReadLevel);
+				}
+				s	= FileManager.fileToArray(sFilename, ++ sReadLevel);
+			}
+			
+			// Create an empty file if the join returns no row
+			File	rsFile	= new File(rsFilename);
+			if(!rsFile.exists()) {
+				rsFile.createNewFile();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static char [] mergeJoinDuplicate(char [] r, char [] s) throws Exception {
+		if(r.length > BUFFER_SIZE || s.length > BUFFER_SIZE) {
+			throw new Exception(MESSAGE_ARRAY_EXCEEDED_BUFFER_SIZE);
+		}
+		
 		int	i	= 0;
 		int	j	= 0;
 		int	k	= 0;
@@ -31,7 +67,7 @@ public class HamaniKhalilMergeJoinDuplicate {
 					j = k;
 				}
 			}
-			else if(r[i] > s[i]) {
+			else if(r[i] > s[j]) {
 				j ++;
 			}
 			else {
---
+++ src/HamaniKhalilTP4/SystemErrors.java
@@ -0,0 +1,12 @@
+package HamaniKhalilTP4;
+
+import static HamaniKhalilTP4.SystemConfiguration.BUFFER_SIZE;
+
+public class SystemErrors {
+	
+	// Error messages
+	public static final String	MESSAGE_ARRAY_EXCEEDED_BUFFER_SIZE		= "One of the two arrays entered as parameters, or both, exceeded the maximum allowed buffer size of " + BUFFER_SIZE;
+	
+	// Error codes
+	
+}
